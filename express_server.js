735a68e745f23cd9d4edbfb2780ec3b598139065
Xihai Luo <xihai.luo01@gmail.com>
2021-11-10 14:30:38 -0500

    create a register template

--- express_server.js
+++ express_server.js
@@ -93,6 +93,11 @@ app.post('/logout', (req, res) => {
   res.redirect('/urls');
 });
 
+/*register a new user*/
+app.get('/register', (req, res) => {
+  res.render('urls_register');
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
